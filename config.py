bd307cabe2be17a7b4439684c7ce74557ea53160
JSeam2 <joshia_seam@mymail.sutd.edu.sg>
2018-05-02 19:44:12 +0800

    Modifications to code to support python3

--- config.py
+++ config.py
@@ -13,23 +13,23 @@ SETTINGS_COLLECTION = DATABASE.settings
 SECRET_KEY = ""
 basedir = os.path.abspath(os.path.dirname(__file__))
 secret_file = os.path.join(basedir, '.secret')
+
 if os.path.exists(secret_file):
     # Read SECRET_KEY from .secret file
-    f = open(secret_file, 'r')
-    SECRET_KEY = f.read().strip()
-    f.close()
+    with open(secret_file, 'rb') as f:
+        SECRET_KEY = f.read().strip()
+
 else:
     # Generate SECRET_KEY & save it away
     SECRET_KEY = os.urandom(24)
-    f = open(secret_file, 'w')
-    f.write(SECRET_KEY)
-    f.close()
-    # Modeify .gitignore to include .secret file
+    with open(secret_file, 'wb') as f:
+        f.write(SECRET_KEY)
+
+    # Modify .gitignore to include .secret file
     gitignore_file = os.path.join(basedir, '.gitignore')
-    f = open(gitignore_file, 'a+')
-    if '.secret' not in f.readlines() and '.secret\n' not in f.readlines():
-        f.write('.secret\n')
-    f.close()
+    with open(gitignore_file, 'a+') as f:
+        if '.secret' not in f.readlines() and '.secret\n' not in f.readlines():
+            f.write('.secret\n')
 
 LOG_FILE = "app.log"
 
